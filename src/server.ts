cc0f014ec7d79e617e1d2a4eb84dc19f802ffd4a
John Popo <enpower56@gmail.com>
2021-05-13 01:52:28 +0800

    Trying for cors

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ app.use(
   })
 );
 app.use(function (req: Request, res: Response, next: NextFunction) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/login"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
