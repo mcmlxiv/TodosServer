3b70adbfb2e7a6e9673a363b9c73002bfd06116d
John Popo <enpower56@gmail.com>
2021-05-13 19:33:19 +0800

    Readjusting models back to JS from errors

--- models/todoList.js
+++ models/todoList.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 //Define Mongoose schema to allow MongoDB to understand incoming data
-const todoListSchema = new Schema({
-  id: String,
-  Date: String,
-  text: String,
-  title: String,
-  todoChange: String,
-  todosActive: Boolean,
-  todosPin: Boolean,
-  userId: String,
-});
+const todoListSchema = new Schema(
+  {
+    id: String,
+    Date: String,
+    text: String,
+    title: String,
+    todoChange: String,
+    todosActive: Boolean,
+    todosPin: Boolean,
+    userId: String,
+  },
+  {
+    collection: "todolist",
+  }
+);
 
 const TodoList = mongoose.model("TodoList", todoListSchema);
 module.exports = TodoList;
--- models/users.js
+++ models/users.js
@@ -2,23 +2,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 //Define Mongoose schema to allow MongoDB to understand incoming data
 
-const userSchema = new Schema({
-  id: String,
-  email: String,
-  password: String,
-  firstName: String,
-  lastName: String,
-  todoList: {
+const userSchema = new Schema(
+  {
     id: String,
-    Date: String,
-    text: String,
-    title: String,
-    todoChange: String,
-    todosActive: Boolean,
-    todosPin: Boolean,
-    userId: String,
+    email: String,
+    password: String,
+    firstName: String,
+    lastName: String,
+    todoList: {
+      id: String,
+      Date: String,
+      text: String,
+      title: String,
+      todoChange: String,
+      todosActive: Boolean,
+      todosPin: Boolean,
+      userId: String,
+    },
   },
-});
+  {
+    collection: "users",
+  }
+);
 
 const Users = mongoose.model("User", userSchema);
 module.exports = Users;
